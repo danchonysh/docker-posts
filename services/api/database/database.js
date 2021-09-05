fe21e196305f964693a8225baa13bcbe8e304baf
daniil.chernyshov <daniil.chernyshov@mobilitygroup.net>
2021-09-06 01:57:11 +0300

    docker-compose in action

--- services/api/database/database.js
+++ services/api/database/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOST } = require('../constants')
 const link = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}/posts?authSource=admin`
+console.log(link)
 
 const createConnection = () =>  mongoose.connect(link, {
 	useNewUrlParser: true,
